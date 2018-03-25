2d2f1eaafe1ad2c7a0e1a71f342abdea0210f50b
Clement Escoffier <clement.escoffier@gmail.com>
2018-03-25 18:46:11 +0200

    initial commit

---
+++ src/main/java/io/openshift/vertx/cache/CacheVerticle.java
@@ -0,0 +1,155 @@
+package io.openshift.vertx.cache;
+
+import io.reactivex.Completable;
+import io.reactivex.Single;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.reactivex.CompletableHelper;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import io.vertx.reactivex.ext.web.client.HttpResponse;
+import io.vertx.reactivex.ext.web.client.WebClient;
+import io.vertx.reactivex.ext.web.handler.BodyHandler;
+import io.vertx.reactivex.ext.web.handler.StaticHandler;
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+public class CacheVerticle extends AbstractVerticle {
+
+    private static final String KEY = "NAME";
+    private RemoteCache<String, String> cache;
+    private WebClient client;
+    private int ttl = 10;
+    private final Logger LOGGER = LoggerFactory.getLogger("Cache-Verticle");
+
+    @Override
+    public void start(Future<Void> future) {
+        ttl = config().getInteger("cache.ttl", 10);
+        // HTTP API
+        Router router = Router.router(vertx);
+        router.route().handler(BodyHandler.create());
+        router.get("/api/greeting").handler(this::greeting);
+        router.get("/api/cached").handler(this::isCached);
+        router.post("/api/ttl").handler(this::setTTL);
+        router.get("/health").handler(rc -> rc.response().end("OK"));
+        router.get("/*").handler(StaticHandler.create());
+
+
+        // Access to Cute name service.
+        client = WebClient.create(vertx, new WebClientOptions()
+            .setDefaultHost("localhost")
+            .setDefaultPort(8081)
+        );
+
+        Completable deployCuteNameService = vertx.rxDeployVerticle(CuteNameService.class.getName())
+            .toCompletable()
+            .doOnComplete(() -> LOGGER.info("Cute Name Service deployed"));
+
+        Completable startHttpServer = vertx
+            .createHttpServer()
+            .requestHandler(router::accept)
+            .rxListen(config().getInteger("http.port", 8080))
+            .toCompletable()
+            .doOnComplete(() -> LOGGER.info("HTTP Server started"));
+
+        deployCuteNameService
+            .andThen(startHttpServer)
+            .subscribe(CompletableHelper.toObserver(future));
+    }
+
+    private void setTTL(RoutingContext rc) {
+        JsonObject body = rc.getBodyAsJson();
+        int newTTL = body.getInteger("ttl", -1);
+        if (newTTL > 0) {
+            rc.response().setStatusCode(400).end("Invalid payload, ttl expected");
+        } else {
+            ttl = newTTL;
+            rc.response()
+                .putHeader("content-type", "application/json")
+                .end(new JsonObject().put("ttl", ttl).encode());
+        }
+    }
+
+    private Single<RemoteCache<String, String>> retrieveCache() {
+        if (cache != null) {
+            return Single.just(cache);
+        } else {
+            return vertx.<RemoteCache<String, String>>rxExecuteBlocking(future ->
+                future.complete(new RemoteCacheManager(
+                    new ConfigurationBuilder().addServer()
+                        .host("cache-server")
+                        .port(11222)
+                        .build())
+                    .getCache()
+                ))
+                .doOnSuccess(c -> {
+                    cache = c;
+                    LOGGER.info("The cache has been retrieved");
+                });
+        }
+    }
+
+    private void isCached(RoutingContext rc) {
+        getCachedValue()
+            .map(Optional::isPresent)
+            .onErrorReturnItem(false)
+            .map(cached -> new JsonObject().put("cached", cached))
+            .map(JsonObject::encode)
+            .subscribe(
+                message -> rc.response()
+                    .putHeader("content-type", "application/json")
+                    .end(message),
+                rc::fail
+            );
+    }
+
+    private void greeting(RoutingContext rc) {
+        getCachedValue()
+            .flatMap(maybe ->
+                maybe.map(Single::just)
+                    .orElse(
+                        client.get("/api/name")
+                            .rxSend()
+                            .map(HttpResponse::bodyAsJsonObject)
+                            .map(j -> j.getString("name"))
+                            .flatMap(this::putInCache)
+                    )
+            )
+            .map(name -> new JsonObject().put("message", "Hello " + name))
+            .onErrorReturn(t -> new JsonObject().put("message", "Unable to call the service: " + t.getMessage()))
+            .map(JsonObject::encode)
+            .subscribe(
+                message -> rc.response()
+                    .putHeader("content-type", "application/json")
+                    .end(message),
+                rc::fail
+            );
+    }
+
+    private Single<String> putInCache(String name) {
+        return vertx
+            .rxExecuteBlocking(future -> {
+                cache.put(KEY, name, ttl, TimeUnit.SECONDS);
+                future.complete();
+            })
+            .map(x -> name);
+    }
+
+    private Single<Optional<String>> getCachedValue() {
+        return retrieveCache()
+            .flatMap(cache ->
+                vertx.<Optional<String>>rxExecuteBlocking(future -> {
+                    String value = cache.get(KEY);
+                    future.complete(Optional.ofNullable(value));
+                }))
+            .doOnError(t -> LOGGER.error("Unable to retrieve the cache", t));
+    }
+}
---
+++ src/test/java/io/openshift/vertx/cache/CacheVerticleIT.java
@@ -0,0 +1,109 @@
+package io.openshift.vertx.cache;
+
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.openshift.client.OpenShiftClient;
+import io.restassured.RestAssured;
+import org.arquillian.cube.openshift.impl.enricher.AwaitRoute;
+import org.arquillian.cube.openshift.impl.enricher.RouteURL;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import static io.restassured.RestAssured.get;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+
+@RunWith(Arquillian.class)
+public class CacheVerticleIT {
+
+    @RouteURL("cache-booster")
+    @AwaitRoute
+    private URL route;
+
+    @ArquillianResource
+    private OpenShiftClient oc;
+
+    @Before
+    public void setup() {
+        RestAssured.baseURI = route.toString();
+
+        await().atMost(5, TimeUnit.MINUTES).until(() -> {
+            List<Pod> list = oc.pods().list().getItems();
+            Optional<Pod> first = list.stream()
+                .filter(p -> p.getMetadata().getName().contains("cache-server")
+                    && !p.getMetadata().getName().contains("deploy"))
+                .filter(p -> p.getStatus().getPhase().contains("Running"))
+                .findFirst();
+            
+            return first.isPresent();
+        });
+
+        System.out.println("JDG ready...");
+
+        await().atMost(5, TimeUnit.MINUTES).catchUncaughtExceptions()
+            .until(() ->
+                get(route).andReturn().statusCode() == 200
+            );
+
+        System.out.println("Application ready...");
+    }
+
+    @Test
+    public void testThatWeAreReady() {
+        await().atMost(5, TimeUnit.MINUTES).catchUncaughtExceptions()
+            .until(() ->
+                get(route).andReturn().statusCode() == 200
+            );
+    }
+
+    @Test
+    public void testCaching() {
+        await().atMost(5, TimeUnit.MINUTES).catchUncaughtExceptions()
+            .until(() ->
+                get(route).andReturn().statusCode() == 200
+            );
+
+        long begin = System.currentTimeMillis();
+
+        System.out.println("Call - " + get("/api/greeting").asString());
+
+        get("/api/greeting").then()
+            .statusCode(equalTo(200));
+        long end = System.currentTimeMillis();
+        long duration1 = end - begin;
+
+        begin = System.currentTimeMillis();
+        get("/api/greeting").then().statusCode(equalTo(200));
+        end = System.currentTimeMillis();
+        long duration2 = end - begin;
+
+        assertThat(duration2).isLessThan(duration1);
+
+        get("/api/cached").then().body("cached", is(true));
+
+        await().atMost(120, TimeUnit.SECONDS)
+            .catchUncaughtExceptions()
+            .until(() ->
+                get("/api/cached").asString().contains("false")
+            );
+
+        get("/api/cached").then().body("cached", is(false));
+
+        begin = System.currentTimeMillis();
+        get("/api/greeting").then().statusCode(equalTo(200));
+        end = System.currentTimeMillis();
+        long duration3 = end - begin;
+
+        assertThat(duration2).isLessThan(duration3);
+    }
+}
