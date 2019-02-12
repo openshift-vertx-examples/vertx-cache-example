f60ed6bab70df900eb20fc8897e595f8d89e7576
Rodney <rdruss@gmail.com>
2019-02-12 12:26:40 -0700

    Allow org to be passed in to test.sh

--- .openshiftio/test.sh
+++ .openshiftio/test.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-source .openshiftio/openshift.sh
-
 if [ ! -d ".openshiftio" ]; then
   warning "The script expects the .openshiftio directory to exist"
   exit 1
 fi
+
+source .openshiftio/openshift.sh
+
+if [ -z "$1" ]; then
+  ORG="openshiftio-vertx-boosters"
+else
+  ORG=$1
+fi
+
+REPO="https://github.com/$ORG/vertx-cache-booster"
+echo -e "\n${YELLOW}Using source repository: $REPO ...\n${NC}"
 
 # cleanup
 oc delete build --all
@@ -34,11 +43,11 @@ oc apply -f greeting-service/.openshiftio/application.yaml
 
 # Create the application
 oc new-app --template=vertx-greeting-service \
-    -p SOURCE_REPOSITORY_URL=https://github.com/openshiftio-vertx-boosters/vertx-cache-booster \
+    -p SOURCE_REPOSITORY_URL="$REPO" \
     -p SOURCE_REPOSITORY_DIR=greeting-service
 
 oc new-app --template=vertx-cute-name-service \
-    -p SOURCE_REPOSITORY_URL=https://github.com/openshiftio-vertx-boosters/vertx-cache-booster \
+    -p SOURCE_REPOSITORY_URL="$REPO" \
     -p SOURCE_REPOSITORY_DIR=cute-name-service
 
 # wait for pods to be ready
